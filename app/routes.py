31fd1581fe88360d4970f155818786b63a57cb01
Will Parker <parkerws3824@gmail.com>
2020-12-22 16:17:34 -0500

    fixed redirect

--- app/routes.py
+++ app/routes.py
@@ -24,7 +24,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
         login_user(user, remember=form.remember_me.data)
     return render_template('login.html', title='Sign in', form=form)
 
